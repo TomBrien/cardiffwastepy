c4a238d20478c7986dc64f209470d3ec8aa6f588
Tom Brien <tom@byth.co.uk>
2022-01-23 21:13:17 +0000

    Fix missing exports

--- cardiffwaste/__init__.py
+++ cardiffwaste/__init__.py
@@ -1,4 +1,9 @@
 """Initiate directory"""
-from cardiffwaste.cardiffwaste import WasteCollections, address_search
+from cardiffwaste.cardiffwaste import (
+    EmptyMatches,
+    Timeout,
+    WasteCollections,
+    address_search,
+)
 
 __version__ = "0.2.1"
